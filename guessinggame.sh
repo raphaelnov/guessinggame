e75ef8fe559fc20097e381764d3fb4452c801ae4
raphnov <raphael.novena@yahoo.com.br>
2017-12-02 14:39:45 -0200

    shell scripted

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,33 +3,31 @@ echo "files are in the current directory. Once the user "
 echo "guess it right, the script is return null and then "
 echo "congratulate the user. Otherwise, it will "
 echo "continuously ask for the right guess."
-
-number_of_files=$(ls -l | grep ^- | wc -l)
-
-#debbuger line
-echo $number_of_files
+echo ""
 
 
-function guess {
-	echo "Tell me your guess: "
-	read response
-	echo $response 
+function guessing {
+	echo "Show me your guess: "
+	echo "Enter your guess and press [ENTER]: "
+	read guess
+}
+number=$(ls -l | grep ^- | wc -l)
+
+
+function giving_tries {
+	check=0
+	while [ "$check" == 0 ]
+	do
+	guessing
+	if [ "$guess" == $number ]
+	then 
+		echo "You guessed it right!"
+		echo "$number"
+		let check=1
+	else  
+		echo "Try it again and be lucky!"
+	fi
+	done
 }
 
-
-guess
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+giving_tries
